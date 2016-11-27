83bc7324029bed88181c7667d79fcf379f0d31ad
aleksandar <93lukic@live.com>
2016-11-28 00:27:49 +0100

    Updated exports

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+module.exports.enums = {};
 module.exports.enums.ConnectionEventEnum = require('./enums/connection/ConnectionEventEnum');
 module.exports.enums.EndpointEventEnum = require('./enums/connection/EndpointEventEnum');
 module.exports.enums.EndpointTypeEnum = require('./enums/connection/EndpointTypeEnum');
 
+
+module.exports.util = {};
 module.exports.util.network = require('./util/network');
