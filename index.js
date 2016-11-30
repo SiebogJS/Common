4d8d97caca2a34e95d351ecc13560cb943df614c
aleksandar <93lukic@live.com>
2016-11-30 23:37:38 +0100

    Added exports

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ module.exports.enums = {};
 module.exports.enums.ConnectionEventEnum = require('./enums/connection/ConnectionEventEnum');
 module.exports.enums.EndpointEventEnum = require('./enums/connection/EndpointEventEnum');
 module.exports.enums.EndpointTypeEnum = require('./enums/connection/EndpointTypeEnum');
+module.exports.enums.MessageTypeEnum = require('./enums/message/MessageTypeEnum');
+module.exports.enums.LayerEnum = require('./enums/LayerEnum');
 
 
 module.exports.util = {};
 module.exports.util.network = require('./util/network');
+
+module.exports.strucs = {};
+module.exports.strucs.Message = require('./structs/Message');
